faaa0a486346727d33b13cc1907d42b401788202
Ahmed Demirezen <47192102+feezx1@users.noreply.github.com>
2019-02-03 13:07:09 +0300

    Update 345_mp4.py

--- 345_mp4.py
+++ 345_mp4.py
@@ -1,3 +1,4 @@
+#içeri aktarilan kutuphaneler
 from bs4 import BeautifulSoup
 import requests
 import csv
@@ -5,6 +6,7 @@ import time
 import urllib
 import os
 import urllib.request
+
 # Baglanacagimiz site
 url = "https://www.ucdortbes.com/videolar/ayt-matematik-soru-bankasi"
 # Siteye baglanti kurmak icin istekte bulunuyoruz
@@ -13,8 +15,8 @@ soup = BeautifulSoup(sayfa,"html.parser")
 #print(soup.prettify)
 print("'' >>> HTML KODU CEKILDI")
 
-#film listesi
-film_list=[]
+#listeler
+liste=[]
 urls=[]
 links=[]
 #parse islemleri
@@ -23,9 +25,9 @@ row_player=body.find("div",{"class":"row row_player"})
 pr_zero=row_player.find("div",{"class":"col-md-4 column pr_zero"})
 scroll=pr_zero.find("div",{"id":"lazy_galeri"})
 for i in scroll.findAll("li"):
-    film_list.append(i)
-for i in range(len(film_list)):
-    for k in film_list[i].findAll("li"):
+    liste.append(i)
+for i in range(len(liste)):
+    for k in liste[i].findAll("li"):
         urls.append(k)
 urls.pop(0)
 for i in range(len(urls)):
